827aa71659a2efdb9f011244c3ecad41aba5a32d
yeesian <ngyeesian@gmail.com>
2015-09-26 04:13:26 -0400

    organize feature classes

--- src/osm_parser.jl
+++ src/osm_parser.jl
@@ -9,49 +9,100 @@
 #   <tag k="source" v="massgis_import_v0.1_20071008165629"/>
 # </node>
 
-type NodeHandle
-    curr::Node
-    nodes::Nodes
+type OSMData
+    nodes::Vector{Node}
+    ways::Vector{Way}
+    relations::Vector{Relation}
+end
+OSMData() = OSMData(Vector{Node}(),Vector{Way}(),Vector{Relation}())
+
+type DataHandle
+    element::Symbol
+    osm::OSMData
+    node::Node # initially undefined
+    way::Way # initially undefined
+    relation::Relation # initially undefined
+
+    DataHandle() = new(:None, OSMData())
+end
+
+function parseElement(handler::LibExpat.XPStreamHandler,
+                      name::String,
+                      attr::Dict{String,String})
+    data = handler.data
+    if name == "node"
+        data.element = :Node
+        data.node = Node(parse(Int, attr["id"]),
+                         (float(attr["lon"]), float(attr["lat"])))
+    elseif name == "way"
+        data.element = :Way
+        data.way = Way(parse(Int, attr["id"]))
+    elseif name == "relation"
+        data.element = :Relation
+        data.relation = Relation(parse(Int, attr["id"]))
+    elseif name == "tag"
+        k = attr["k"]; v = attr["v"]
+        (data.element == :Node) && (data_tags = tags(data.node))
+        (data.element == :Way) && (data_tags = tags(data.way))
+        (data.element == :Relation) && (data_tags = tags(data.relation))
+        data_tags[k] = v
+    elseif name == "nd"
+        push!(data.way.nodes, parse(Int, attr["ref"]))
+    elseif name == "member"
+        push!(data.relation.members, attr)
+    end
+end
+
+function collectElement(handler::LibExpat.XPStreamHandler, name::String)
+    if name == "node"
+        push!(handler.data.osm.nodes, handler.data.node)
+        handler.data.element = :None
+    elseif name == "way"
+        push!(handler.data.osm.ways, handler.data.way)
+        handler.data.element = :None
+    elseif name == "relation"
+        push!(handler.data.osm.relations, handler.data.relation)
+        handler.data.element = :None
+    end
+end
+
+function parseOSM(filename::String; args...)
+    callbacks = LibExpat.XPCallbacks()
+    callbacks.start_element = parseElement
+    callbacks.end_element = collectElement
+    data = DataHandle()
+    LibExpat.parsefile(filename, callbacks, data=data; args...)
+    data.osm::OSMData
 end
 
 function parseNode(handler::LibExpat.XPStreamHandler,
                    name::String,
                    attr::Dict{String,String})
-    curr = handler.data.curr::Node
+    data = handler.data
     if name == "node"
-        curr.element = :Node
-        curr.id = parse(Int, attr["id"])
-        curr.latlon = (float(attr["lat"]), float(attr["lon"]))
-    elseif name == "tag" && curr.element == :Node
-        curr.tags[attr["k"]] = attr["v"]
+        data.element = :Node
+        data.curr = Node(parse(Int, attr["id"]),
+                         (float(attr["lon"]), float(attr["lat"])),
+                         Dict{UTF8String,UTF8String}())
+    elseif name == "tag" && data.element == :Node
+        data.curr.tags[attr["k"]] = attr["v"]
     end
 end
 
 function collectNode(handler::LibExpat.XPStreamHandler, name::String)
-    nodes = handler.data.nodes::Nodes
-    curr = handler.data.curr::Node
     if name == "node"
-        nodes.latlon[curr.id] = curr.latlon
-        push!(nodes.ids, curr.id)
-        for key in keys(curr.tags)
-            if !haskey(nodes.tags, key)
-                nodes.tags[key] = Dict{Int,UTF8String}()
-            end
-            nodes.tags[key][curr.id] = curr.tags[key]
-        end
-        # reset element
-        curr.element = :None
-        curr.tags = Dict()
+        handler.data.element = :None
+        push!(handler.data.nodes, handler.data.curr)
     end
 end
 
 function parseNodes(filename::String; args...)
-    nodehandle = NodeHandle(Node(), Nodes())
     callbacks = LibExpat.XPCallbacks()
     callbacks.start_element = parseNode
     callbacks.end_element = collectNode
+    nodehandle = NodeHandle()
     LibExpat.parsefile(filename, callbacks, data=nodehandle; args...)
-    nodehandle.nodes::Nodes
+    nodehandle.nodes::Vector{Node}
 end
 
 # Example (Way)
@@ -69,51 +120,43 @@ end
 #   </way>
 
 type WayHandle
-    curr::Way
-    ways::Ways
+    element::Symbol
+    ways::Vector{Way}
+    curr::Way # initially undefined
+    WayHandle(element::Symbol, ways::Vector{Way}) = new(element, ways)
 end
 
 function parseWay(handler::LibExpat.XPStreamHandler,
                   name::String,
                   attr::Dict{String,String})
-    curr = handler.data.curr::Way
     if name == "way"
-        curr.element = :Way
-        curr.id = parse(Int, attr["id"])
-    elseif curr.element == :Way
+        handler.data.element = :Way
+        handler.data.curr = Way(parse(Int, attr["id"]),
+                                Vector{Int}(),
+                                Dict{UTF8String,UTF8String}())
+    elseif handler.data.element == :Way
         if name == "tag"
-            curr.tags[attr["k"]] = attr["v"]
+            handler.data.curr.tags[attr["k"]] = attr["v"]
         elseif name == "nd"
-            push!(curr.nodes, parse(Int, attr["ref"]))
+            push!(handler.data.curr.nodes, parse(Int, attr["ref"]))
         end
     end
 end
 
 function collectWay(handler::LibExpat.XPStreamHandler, name::String)
-    ways = handler.data.ways::Ways
-    curr = handler.data.curr::Way
     if name == "way"
-        push!(ways.ids, curr.id)
-        ways.nodes[curr.id] = curr.nodes
-        for key in keys(curr.tags)
-            if !haskey(ways.tags, key)
-                ways.tags[key] = Dict{Int,UTF8String}()
-            end
-            ways.tags[key][curr.id] = curr.tags[key]
-        end
-        curr.element = :None
-        curr.nodes = Int[]
-        curr.tags = Dict()
+        push!(handler.data.ways, handler.data.curr)
+        handler.data.element = :None
     end
 end
 
 function parseWays(filename::String; args...)
-    wayhandle = WayHandle(Way(), Ways())
+    wayhandle = WayHandle(:None, Vector{Way}())
     callbacks = LibExpat.XPCallbacks()
     callbacks.start_element = parseWay
     callbacks.end_element = collectWay
     LibExpat.parsefile(filename, callbacks, data=wayhandle; args...)
-    wayhandle.ways::Ways
+    wayhandle.ways::Vector{Way}
 end
 
 # Example (Relation)
@@ -127,100 +170,41 @@ end
 # </relation>
 
 type RelationHandle
-    curr::Relation
-    relations::Relations
+    element::Symbol
+    relations::Vector{Relation}
+    curr::Relation # initially undefined
+    RelationHandle(e::Symbol, relations::Vector{Relation}) = new(e, relations)
 end
 
 function parseRelation(handler::LibExpat.XPStreamHandler,
                        name::String,
                        attr::Dict{String,String})
-    curr = handler.data.curr::Relation
     if name == "relation"
-        curr.element = :Relation
-        curr.id = parse(Int, attr["id"])
-    elseif curr.element == :Relation
+        handler.data.element = :Relation
+        handler.data.curr = Relation(parse(Int, attr["id"]),
+                                     Vector{Dict{UTF8String,UTF8String}}(),
+                                     Dict{UTF8String,UTF8String}())
+    elseif handler.data.element == :Relation
         if name == "tag"
-            curr.tags[attr["k"]] = attr["v"]
+            handler.data.curr.tags[attr["k"]] = attr["v"]
         elseif name == "member"
-            push!(curr.members, attr)
+            push!(handler.data.curr.members, attr)
         end
     end
 end
 
 function collectRelation(handler::LibExpat.XPStreamHandler, name::String)
-    relations = handler.data.relations::Relations
-    curr = handler.data.curr::Relation
     if name == "relation"
-        push!(relations.ids, curr.id)
-        relations.members[curr.id] = curr.members
-        for key in keys(curr.tags)
-            if !haskey(relations.tags, key)
-                relations.tags[key] = Dict{Int,UTF8String}()
-            end
-            relations.tags[key][curr.id] = curr.tags[key]
-        end
-        curr.element = :None
-        curr.members = Dict{UTF8String,UTF8String}[]
-        curr.tags = Dict()
+        push!(handler.data.relations, handler.data.curr)
+        handler.data.element = :None
     end
 end
 
 function parseRelations(filename::String; args...)
-    relationhandle = RelationHandle(Relation(), Relations())
+    relationhandle = RelationHandle(:None, Vector{Relation}())
     callbacks = LibExpat.XPCallbacks()
     callbacks.start_element = parseRelation
     callbacks.end_element = collectRelation
     LibExpat.parsefile(filename, callbacks, data=relationhandle; args...)
-    relationhandle.relations::Relations
+    relationhandle.relations::Vector{Relation}
 end
-
-# ### PARSE OSM ENTITIES ###
-
-# function parse_highway(attr::OSMattributes, k::String, v::String)
-#     if k == "highway"
-#         attr.class = v
-#         if v == "services" # Highways marked "services" are not traversable
-#             attr.visible = false
-#             return
-#         end
-#         if v == "motorway" || v == "motorway_link"
-#             attr.oneway = true # motorways default to oneway
-#         end
-#     elseif k == "oneway"
-#         if v == "-1"
-#             attr.oneway = true
-#             attr.oneway_reverse = true
-#         elseif v == "false" || v == "no" || v == "0"
-#             attr.oneway = false
-#             attr.oneway_override = true
-#         elseif v == "true" || v == "yes" || v == "1"
-#             attr.oneway = true
-#         end
-#     elseif k == "junction" && v == "roundabout"
-#         attr.oneway = true
-#     elseif k == "cycleway"
-#         attr.cycleway = v
-#     elseif k == "sidewalk"
-#         attr.sidewalk = v
-#     elseif k == "bicycle"
-#         attr.bicycle = v
-#     elseif k == "lanes" && length(v)==1 && '1' <= v[1] <= '9'
-#         attr.lanes = int(v)
-#     else
-#         return
-#     end
-#     attr.parent = :Highway
-# end
-
-# function parse_building(attr::OSMattributes, v::String)
-#     attr.parent = :Building
-#     if isempty(attr.class)
-#         attr.class = v
-#     end
-# end
-
-# function parse_feature(attr::OSMattributes, k::String, v::String)
-#     attr.parent = :Feature
-#     attr.class = k
-#     attr.detail = v
-# end
---
+++ src/osm_rules.jl
@@ -0,0 +1,76 @@
+# classification
+building(w::Way) = haskey(w.tags, "building")
+highway(w::Way) = haskey(w.tags, "highway")
+
+# predicates
+visible{T <: OSMElement}(obj::T) = (get(obj.tags, "visible", "") != "false")
+services(w::Way) = (get(w.tags,"highway", "") == "services")
+reverse(w::Way) = (get(w.tags,"oneway", "") == "-1")
+
+function haslanes(w::Way)
+    if haskey(w.tags, "lanes")
+        v = w.tags["lanes"]
+        return length(v)==1 && '1' <= v[1] <= '9'
+    end
+    return false
+end
+
+function oneway(w::Way)
+    v = get(w.tags,"oneway", "")
+
+    if v == "false" || v == "no" || v == "0"
+        return false
+    elseif v == "-1" || v == "true" || v == "yes" || v == "1"
+        return true
+    end
+
+    highway = get(w.tags,"highway", "")
+    junction = get(w.tags,"junction", "")
+
+    return (highway == "motorway" ||
+            highway == "motorway_link" ||
+            junction == "roundabout")
+end
+
+# attributes
+lanes(w::Way) = parse(Int, w.tags["lanes"])
+
+# feature class
+function roadway(w::Way)
+    if highway(w) && haskey(ROAD_CLASSES, w.tags["highway"])
+        return ROAD_CLASSES[w.tags["highway"]]
+    end
+    return 0
+end
+
+function walkway(way::Way)
+    sidewalk = get(way.tags, "sidewalk", "")
+    if sidewalk != "no"
+        if haskey(PED_CLASSES, "sidewalk:$(sidewalk)")
+            return PED_CLASSES["sidewalk:$(sidewalk)"]
+        elseif haskey(PED_CLASSES, way.tags["highway"])
+            return PED_CLASSES[way.tags["highway"]]
+        end
+    end
+    return 0
+end
+
+function cycleway(w::Way)
+    bicycle = get(w.tags, "bicycle", "")
+    cycleway = get(w.tags, "cycleway", "")
+    highway = get(w.tags, "highway", "")
+
+    cycleclass = "cycleway:$(cycleway)"
+    bikeclass = "bicycle:$(bicycle)"
+
+    if bicycle != "no"
+        if haskey(CYCLE_CLASSES, cycleclass)
+            return CYCLE_CLASSES[cycleclass]
+        elseif haskey(CYCLE_CLASSES, bikeclass)
+            return CYCLE_CLASSES[bikeclass]
+        elseif haskey(CYCLE_CLASSES, highway)
+            return CYCLE_CLASSES[highway]
+        end
+    end
+    return 0
+end
