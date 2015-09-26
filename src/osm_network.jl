077676dc22c8533360be70b96118fd33eaf3f076
yeesian <ngyeesian@gmail.com>
2015-09-26 12:38:11 -0400

    switch to a sparse matrix representation of the distance matrix

--- src/osm_network.jl
+++ src/osm_network.jl
@@ -1,16 +1,8 @@
-
-type RoadDist{T} <: AbstractArray{T, 2}
-    dist::Dict{Tuple{Int,Int}, T}
-    shape::Tuple{Int,Int}
-end
-Base.getindex(w::RoadDist, s::Int, d::Int) = w.dist[(s,d)]
-Base.size(::RoadDist) = (typemax(Int), typemax(Int))
-
 type Network
     g::LightGraphs.DiGraph
-    osm_id::Vector{Int}     # Graph ID (1:n) -> OSM ID
-    node_id::Dict{Int,Int}  # OSM ID -> Graph ID
-    w::RoadDist             # Edge weights, indexed by Graph ID
+    osm_id::Vector{Int}                     # Graph ID (1:n) -> OSM ID
+    node_id::Dict{Int,Int}                  # OSM ID -> Graph ID
+    distmx::SparseMatrixCSC{Float64, Int}   # Edge weights, indexed by Graph ID
 end
 
 "fetch all osm ids from the list of osm elements"
@@ -32,55 +24,49 @@ function node_indices(osm_id::Vector{Int})
     node_id
 end
 
-function numedges(ways::Vector{Way})
-    n = -length(ways)
-    for way in ways
-        n += length(way.nodes)
-    end
-    n
+toradians(degree::Float64) = degree * π / 180.0
+todegrees(radian::Float64) = radian * 180.0 / π
+
+"distance between the two points in kilometres"
+function distance(pt1::Tuple{Float64, Float64}, pt2::Tuple{Float64, Float64})
+    dLat = toradians(pt2[2] - pt1[2])
+    dLon = toradians(pt2[1] - pt1[1])
+    lat1 = toradians(pt1[2])
+    lat2 = toradians(pt2[2])
+    a = sin(dLat/2)^2 + sin(dLon/2)^2 * cos(lat1) * cos(lat2)
+    2.0 * atan2(sqrt(a), sqrt(1-a)) * 6373.0
 end
 
-function createNetwork(osm::OSMData) #, relations::Relations)
-    wgs84 = Proj4.Projection(Proj4.epsg[4269])
-    nodes = osm.nodes
-    ways = osm.ways
+function osm2digraph(osm::OSMData) #, relations::Relations)
+    nodes = osm.nodes; ways = osm.ways
     numnodes = length(nodes)
     osm_id = osm_indices(nodes)
     node_id = node_indices(osm_id)
-
-    g = LightGraphs.DiGraph(numnodes)
-    w = RoadDist(Dict{Tuple{Int,Int}, Float64}(), (numnodes,numnodes))
-    sizehint!(w.dist, numedges(ways))
     
     ways = ways[map(visible, ways) & ~(map(services, ways))]
     rev_ways = map(reverse, ways)
     directed_ways = map(oneway, ways)
+    edges = Set{Tuple{Int,Int}}()
 
     for i in 1:length(ways)
         way = ways[i]
         rev, nrev = rev_ways[i], !rev_ways[i]
-        directed = directed_ways[i]
-
         for n in 2:length(way.nodes)
-            n0 = node_id[way.nodes[n-1]]
+            n0 = node_id[way.nodes[n-1]] # map osm_id -> node_id
             n1 = node_id[way.nodes[n]]
-            startnode = n0*nrev + n1*rev
+            startnode = n0*nrev + n1*rev # reverse the direction if need be
             endnode = n0*rev + n1*nrev
-            
-            startpt = nodes[startnode].lonlat
-            endpt = nodes[endnode].lonlat
-            distance = abs(Proj4.geod_distance(startpt, endpt, wgs84))
-
-            if !LightGraphs.has_edge(g, startnode, endnode)
-                LightGraphs.add_edge!(g, startnode, endnode)
-                w.dist[(startnode, endnode)] = distance
-            end
-            if !directed && !LightGraphs.has_edge(g, endnode, startnode)
-                LightGraphs.add_edge!(g, endnode, startnode)
-                w.dist[(endnode, startnode)] = distance # proxy
-            end
+            push!(edges, (startnode, endnode))
+            !directed_ways[i] && push!(edges, (endnode, startnode))
         end
     end
 
-    Network(g, osm_id, node_id, w)
+    edges = reinterpret(Int, collect(edges))
+    I = edges[1:2:end] # collect all start nodes
+    J = edges[2:2:end] # collect all end nodes
+    V = Float64[distance(nodes[I[i]].lonlat, nodes[J[i]].lonlat)
+                for i in 1:length(I)]
+    distmx = sparse(I, J, V, numnodes, numnodes)
+
+    Network(LightGraphs.DiGraph(distmx), osm_id, node_id, distmx)
 end
